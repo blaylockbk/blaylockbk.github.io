10fa3109963ea109d07c4ea856d113d0e0e6c864
Brian Blaylock <blaylockbk@users.noreply.github.com>
2022-02-28 10:21:51 -0800

    update

--- js/site/siteclose.js
+++ js/site/siteclose.js
@@ -3,9 +3,9 @@ document.write('\
 <div id="footer">\
 <p style="text-align:center;color:#990000">\
 © Copyright 2022, Brian K. Blaylock. \
- <a href="https://twitter.com/blaylockbk" style="text-decoration:none"><big><i class="fab fa-twitter fa-fw"></i></big> </a>\
- <a href="https://github.com/blaylockbk" style="text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
- <a href="https://github.com/blaylockbk/blaylockbk.github.io/issues"><big><i class="fas fa-envelope fa-fw"></i></big> contact</a>\
+ <a href="https://twitter.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-twitter fa-fw"></i></big> </a>\
+ <a href="https://github.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
+ <a href="https://github.com/blaylockbk/blaylockbk.github.io/issues" style="color:#990000; text-decoration:none"><big><i class="fas fa-envelope fa-fw"></i></big> contact</a>\
 </p>\
 </div>\
 <script src="js/site/googleanalytics.js"></script>\
