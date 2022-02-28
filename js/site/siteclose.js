e41609a69f91d2104d7166abf4e8c5654cf42f0d
Brian Blaylock <blaylockbk@users.noreply.github.com>
2022-02-28 10:15:02 -0800

    update footer

--- js/site/siteclose.js
+++ js/site/siteclose.js
@@ -2,11 +2,10 @@ document.write('\
 <br>\
 <div id="footer">\
 <p style="text-align:center;color:#990000">\
-This page is maintained by University of Utah &copy 2020, \
- <a href="http://www.atmos.utah.edu/" style="color:#990000; text-decoration:none">Department of Atmospheric Science, University of Utah</a>. \
- <a href="https://twitter.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-twitter fa-fw"></i></big> </a>\
- <a href="https://github.com/blaylockbk" style="color:#990000; text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
- <big><i class="fas fa-envelope fa-fw"></i></big> atmos-mesowest [at] lists.utah.edu\
+© Copyright 2022, Brian K. Blaylock. \
+ <a href="https://twitter.com/blaylockbk" style="text-decoration:none"><big><i class="fab fa-twitter fa-fw"></i></big> </a>\
+ <a href="https://github.com/blaylockbk" style="text-decoration:none"><big><i class="fab fa-github fa-fw"></i></big> </a>\
+ <a href="https://github.com/blaylockbk/blaylockbk.github.io/issues"><big><i class="fas fa-envelope fa-fw"></i></big> contact</a>\
 </p>\
 </div>\
 <script src="js/site/googleanalytics.js"></script>\
